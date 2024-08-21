516c39c9f5a7e5e112ac0b024423f659313156f4
gustavo mota <145623580+GustavoAlMota@users.noreply.github.com>
2024-08-21 12:14:40 -0300

    Add files via upload

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,9 +4,24 @@ import BannerMovies from '../../components/bannerFilmes';
 import CardMovies from '../../components/cardMovies';
 import Header from '../../components/header';
 import SearchBar from '../../components/searchbar';
-import Filmes from '../../data/movies'
+import Filmes from '../../data/movies';
+
+import {useEffect, useState} from 'react';
+
 
 export default function App() {
+  const [movies ,setMovies] = useState();
+
+  useEffect(()=>{
+    async function buscarFilmes(){
+    const response = await fetch ('https://api.themoviedb.org/3/movie/now_playing?api_key=a96ce4782868517e1a1e0690ae3bcbe2&language=pt-BR&page=1')
+    const data = await response.json()
+    console.log(data.results)
+    }
+  
+  buscarFilmes();
+}
+  ,[])
   return (
     <View style={styles.container}>
      <Header></Header>
